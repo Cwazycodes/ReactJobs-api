67731a34d37445c54f159cd34ea949b0c405b10e
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-08-25 22:52:05 +0100

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./app');
+const app = require("./app");
 
-const Port = process.env.PORT || 5000;
+const Port = process.env.PORT || 7080;
 app.listen(Port, () => {
   console.log(`Server is running on port ${Port}`);
 });
