fd8198eaa7083d6106ebf2d42f53394971ed0910
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-08-25 21:49:48 +0100

    fixed the ports

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const Port = process.env.PORT || 5000;
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
 });
